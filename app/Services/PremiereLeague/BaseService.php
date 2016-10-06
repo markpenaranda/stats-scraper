89a20edcfb5aae3ab26356fd30dfbad7543b28c9
MJP <markangelpenaranda@gmail.com>
2016-10-06 18:10:44 +0800

    stats

--- app/Services/PremiereLeague/BaseService.php
+++ app/Services/PremiereLeague/BaseService.php
@@ -34,6 +34,7 @@ class BaseService {
 
 	    $timeout = $this->timeout;
 	   	while ($response->getStatus() != 200) {
+	   		dump($url . " @" . $timeout);
 	   		$timeout += 5000;
 	   		$request->setTimeout($timeout);
 	   		
