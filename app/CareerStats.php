cdfb67f8eb7018a9f1160eac9d98b068006357af
MJP <markangelpenaranda@gmail.com>
2016-11-24 21:33:47 +0800

    Epl Roster Fix add Fillable

--- app/CareerStats.php
+++ app/CareerStats.php
@@ -8,7 +8,7 @@ class CareerStats extends Model
 {
     protected $table = "career_stats";
 
-    protected $visible = ['total_stats'];
+    protected $visible = ['total_stats', 'player_id'];
     public function getTotalStatsAttribute($value) 
     {
     	return json_decode($value);
