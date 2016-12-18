8529146b72cc45f4ed3c0654fac04d4394c5ff32
MJP <markangelpenaranda@gmail.com>
2016-12-18 21:39:59 +0800

    From Fix

--- config/mail.php
+++ config/mail.php
@@ -55,10 +55,7 @@ return [
     |
     */
 
-    'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
-    ],
+    'from' => ['address' => 'support@drafthoops.com', 'name' => 'Drafthoops Customer Support'],
 
     /*
     |--------------------------------------------------------------------------
