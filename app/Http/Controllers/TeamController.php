66a56817440de5c0d1b18ae88a9f39e9104a2c3f
MJP <markangelpenaranda@gmail.com>
2017-01-18 16:13:33 +0800

    Player

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Team;
+use App\Player;
 class TeamController extends Controller
 {
 
@@ -30,13 +31,14 @@ class TeamController extends Controller
     public function showRoster(Request $request, $id)
     {
     	$team = Team::find($id);
+        $roster = Player::where('active', 1)->with('season_stats')->where('team_id', $team->id)->get()->toArray();
     	$item = [
     		'name' => $team->name,
     		'image_url' => $team->image_url,
     		'id' => $team->id,
     		'league' => $team->league,
     		'abbreviation' => $team->abbreviation,
-    		'roster' => $team->roster->where('active', 1)->load('season_stats')
+    		'roster' => $roster
     	];
 
     	return response()->json($item);
