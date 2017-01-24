c8305c53132046a4b230a7fd13c103c3013d9d31
MJP <markangelpenaranda@gmail.com>
2017-01-24 14:38:29 +0800

    Test

--- app/Console/Commands/Data/EPL/EplGameStats.php
+++ app/Console/Commands/Data/EPL/EplGameStats.php
@@ -48,12 +48,12 @@ class EplGameStats extends Command
       $eplPlayer = new EplPlayer();
 
 
-        $startDate = strtotime(date("Y-m-d") . " -1 days") * 1000;
+        $startDate = strtotime(date("Y-m-d") . " -3 days") * 1000;
         $endDate = strtotime(date("Y-m-d") . " +1 days") * 1000;
 
         $this->info($startDate);
         $this->info($endDate);
-        
+
         $matches = Match::where('schedule', '>', $startDate)->where('schedule', '<',  $endDate)->where('status', '!=', "Final")->where('league', 'epl')->get();
 
         
